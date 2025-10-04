bf3ca28ed6eb415d0b96e2019dfe6112875298e4
lsy8658 <socszz@gmail.com>
2025-10-04 18:45:52 +0900

    언패킹과 예외처리

--- study/01.py
+++ study/01.py
@@ -24,3 +24,15 @@
 #==============================================
 # 딕셔너리 => 객체라 보면됨 키와 값으로 이루어진 쌍의 집합
 
+# city_population = {
+#     '서울': 957, '부산': 339, '인천': 294, '대구': 242, '광주': 145, '대전': 147,
+#     '울산': 114, '세종': 36, '수원': 115, '창원': 103, '고양': 105, '용인': 108, '성남': 94
+# }
+
+# 인구 300만 이상 및 이름에 '산'이 포함된 도시 리스트
+
+# large_short_name_cities = []
+# for i in city_population:
+#     if '산' in i :
+#         large_short_name_cities.append(city_population[i])
+# print(large_short_name_cities)
